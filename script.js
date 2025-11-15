45577fa89f4e9e7be898000bd6c68f2a664b501d
zaza1234deffvf <101509809+zaza1234deffvf@users.noreply.github.com>
2025-11-15 12:57:22 -0500

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-var xhr = new XMLHttpRequest();
-xhr.open("POST", "https://discordapp.com/api/webhooks/1439301765223809054/wnIEKKbQ8ncRvNdmKa1XaOMtaSrRZ1_VfKBUwpp_DqeYRSTGpECePunmRlO0qzt8IWKC", true);
-xhr.setRequestHeader('Content-Type', 'application/json');
-xhr.send(JSON.stringify({
-  "content": "New visitor from IP " + window.navigator.userAgent + " and User Agent: " + window.location.href
-}));
